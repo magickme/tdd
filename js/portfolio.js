381459d77681582607afa2e787fd16d56a900385
Jason Louv <jason@ultraculture.org>
2022-05-29 19:43:39 -0500

    feat: Improved error handling for missing exchange rates

--- js/portfolio.js
+++ js/portfolio.js
@@ -9,11 +9,19 @@ class Portfolio {
         this.moneys = this.moneys.concat(moneys);
     }
     evaluate(currency) {
+        let failures = [];
         let total = this.moneys.reduce((sum, money) => {
-                return sum + this.convert(money, currency);
-            },
-            0);
-        return new Money(total, currency);
+            let convertedAmount = this.convert(money, currency);
+            if (convertedAmount === undefined) {
+                failures.push(money.currency + "->" + currency);
+                return sum;
+            }
+            return sum + convertedAmount;
+        }, 0);
+        if (!failures.length) {
+            return new Money(total, currency);
+        }
+        throw new Error("Missing exchange rate(s):[" + failures.join() + "]");
     };
     convert(money, currency) {
         let exchangeRates = new Map();
@@ -23,7 +31,11 @@ class Portfolio {
             return money.amount;
         };
         let key = money.currency + "->" + currency;
-        return money.amount * exchangeRates.get(key);
+        let rate = exchangeRates.get(key);
+        if (rate === undefined) {
+            return undefined;
+        }
+        return money.amount * rate;
     };
 };
 
